cc9249f3fd6663ef11de24405581ec9b90bc33d5
lucrain 11 <lucrain 11@192.168.2.7>
2012-09-17 10:22:23 +0530

    3rd commit

---
+++ src/my/java/app/numberPyramid/NumberPyramid.java
@@ -0,0 +1,20 @@
+package my.java.app.numberPyramid;
+
+public class NumberPyramid {
+	public static void main(String[] args)
+	{
+		int j;
+		int k=1;
+		
+		for(j=0; j<=10; j++)
+		{
+			System.out.println();
+		for(int i=j; i<=10; i++)
+		{
+			System.out.print(k++);
+		}
+		
+		}	
+	}
+
+}
